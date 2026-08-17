2871bdbff14ad3d315ae559dc241a9fffacad6e0
umfrancisco <francis.guitler@gmail.com>
2026-08-17 10:43:47 -0300

    Fix language icons url

--- src/components/RepoList/index.jsx
+++ src/components/RepoList/index.jsx
@@ -21,16 +21,16 @@ const RepoList = () => {
 
     function getImgUrl(language) {
         if (language === "Java") {
-            return "https://raw.githubusercontent.com/umfrancisco/developer-portfolio-react/refs/heads/main/src/assets/java-icon.svg";
+            return "src/assets/java-icon.svg";
         }
         if (language === "JavaScript") {
-            return "https://raw.githubusercontent.com/umfrancisco/developer-portfolio-react/refs/heads/main/src/assets/javascript-icon.svg";
+            return "src/assets/javascript-icon.svg";
         }
         if (language === "Go") {
-            return "https://raw.githubusercontent.com/umfrancisco/developer-portfolio-react/refs/heads/main/src/assets/go-icon.svg";
+            return "src/assets/go-icon.svg";
         }
         if (language === "TypeScript") {
-            return "https://raw.githubusercontent.com/umfrancisco/developer-portfolio-react/refs/heads/main/src/assets/typescript-icon.svg";
+            return "src/assets/typescript-icon.svg";
         }
         return "unknown";
     }
