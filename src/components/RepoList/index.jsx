bfad728d92f9526ba94e188767d31e43b5bccc2b
umfrancisco <francis.guitler@gmail.com>
2026-08-30 12:26:03 -0300

    Add loading animation

--- src/components/RepoList/index.jsx
+++ src/components/RepoList/index.jsx
@@ -4,6 +4,18 @@ import { projects } from "../../data";
 
 const RepoList = () => {
 
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://staging-repo-projects-api-r77i.encr.app/projects")
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+                setLoading(false);
+            });
+    }, [])
+
     function getImgUrl(language) {
         if (language === "Java") {
             return "https://raw.githubusercontent.com/umfrancisco/developer-portfolio-react/refs/heads/main/src/assets/java-icon.svg";
@@ -20,12 +32,21 @@ const RepoList = () => {
         return "unknown";
     }
 
+    if (loading) {
+        
+        return (
+            <div className="container">
+                <div class="loader"></div>
+            </div>
+        )
+    }
+
     return (
         <div className="container">
             <h4 className={styles.title}>Selected Projects</h4>
             <ul className={styles.list}>
-                {projects.map(project => (
-                    <li className={styles.listItem} key={project.id}>
+                {data.projects.map(project => (
+                    <li className={styles.listItem} key={project.name}>
                         <img className={styles.langIcon} src={getImgUrl(project.language)} alt={project.language} />
                         <div className={styles.repoContainer}>
                             <div className={styles.itemName}>
